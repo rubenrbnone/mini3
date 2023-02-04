156f6aa46b420e4b7b3511c325dd5de435aac70d
rubenrbnone <ruben@rbn.one>
2023-02-04 00:44:57 -0500

    Done

--- application/config/config.php
+++ application/config/config.php
@@ -54,8 +54,8 @@ define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
  * This is the place where you define your database credentials, database type etc.
  */
 define('DB_TYPE', 'mysql');
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'mini');
-define('DB_USER', 'root');
-define('DB_PASS', '12345678');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'apio');
+define('DB_USER', 'ruben');
+define('DB_PASS', 'ruben');
 define('DB_CHARSET', 'utf8');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'panique/mini3',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '30d1baa96e2b8e2edce674bf3b31a04df535b673',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'panique/mini3' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '30d1baa96e2b8e2edce674bf3b31a04df535b673',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
